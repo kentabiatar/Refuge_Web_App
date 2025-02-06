b34040baeb5a18fcc5e41a7c4c37e85e9ce8f2c1
Kent Abiatar <kentabiatar@gmail.com>
2025-02-06 12:30:20 +0700

    initialized the frontend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json({limit: "5mb"})); //to parse req.body
+app.use(express.json({limit: "5mb"})); //to parse req.body (for img upload max 5mb)
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
