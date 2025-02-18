e8a59ac30435e7e77fabe503bcd5994f82822869
Kent Abiatar <kentabiatar@gmail.com>
2025-02-18 19:25:49 +0700

    deploymeny is hard

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 }));
 app.use(express.json({limit: "5mb"})); //to parse req.body (for img upload max 5mb)
 app.use(cookieParser());
