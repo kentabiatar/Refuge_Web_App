021abd5d99dc9569042adca196203491665aa09d
Kent Abiatar <kentabiatar@gmail.com>
2025-02-18 20:16:08 +0700

    im done for real

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [process.env.FRONTEND_URL,"https://refuge-swart.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
 }));
