c128aab7043e49e11144daa556a14f3caa52d5b2
Kent Abiatar <kentabiatar@gmail.com>
2025-02-18 17:44:31 +0700

    fix merge issues

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-if(process.env.NODE_ENV !== "production"){
-    app.use(cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    }));
-}
+app.use(cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+}));
+
 
 
 app.use(express.json({limit: "5mb"})); //to parse req.body (for img upload max 5mb)
@@ -33,13 +32,6 @@ app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/posts", postRoutes);
 app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")))
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-    })
-}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
