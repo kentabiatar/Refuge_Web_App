94f36fb055321bdbb78a89f174bae75b042815ff
Kent Abiatar <kentabiatar@gmail.com>
2025-02-18 18:03:17 +0700

    testing somethign

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "https://refuge-swart.vercel.app",
     credentials: true,
 }));
 
