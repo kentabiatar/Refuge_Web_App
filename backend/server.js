9fc2ecc6753bf8bd337f71bebe3f1bb53f13ca3f
Kent Abiatar <kentabiatar@gmail.com>
2025-02-18 18:46:42 +0700

    another testing

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import path from "path";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js"; 
@@ -15,15 +14,11 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 
 app.use(cors({
     origin: "https://refuge-swart.vercel.app",
     credentials: true,
 }));
-
-
-
 app.use(express.json({limit: "5mb"})); //to parse req.body (for img upload max 5mb)
 app.use(cookieParser());
 
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosClient = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:5000/api/v1",
+    baseURL: "http://localhost:5000/api/v1",
     withCredentials: true,
 });
 
